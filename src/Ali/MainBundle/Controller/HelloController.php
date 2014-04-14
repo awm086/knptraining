fe0b8d118486a199304eef11f8cdad7244f84747
Ali Mustafa <Ali.Almustafa@nbcuni.com>
2014-04-14 14:18:36 -0400

    adding product bundle

---
+++ src/Ali/MainBundle/Controller/HelloController.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: ali
+ * Date: 4/14/14
+ * Time: 11:20 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+// src/Acme/HelloBundle/Controller/HelloController.php
+
+namespace Ali\MainBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class HelloController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render(
+            'MainBundle:Hello:index.html.twig',
+            array('name' => $name));
+    }
+}
---
+++ src/Ali/MainBundle/MainBundle.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: ali
+ * Date: 4/14/14
+ * Time: 10:53 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+namespace Ali\MainBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class MainBundle extends Bundle
+{
+
+}
